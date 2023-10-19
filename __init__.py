7154332ce419dcc20f37045892b68e85ac57c551
Alexander Zeier <alexander.zeier@outlook.de>
2023-10-19 10:44:38 +0200

    api call added

--- __init__.py
+++ __init__.py
@@ -1,15 +1,43 @@
-from mycroft import MycroftSkill, intent_file_handler
+from va import MycroftSkill, intent_handler
+from va.adapt.intent import IntentBuilder
+from va.skills.context import adds_context, removes_context
+from va.dataModel import UserSkillMetis
+from va import Message
+
+import requests
+
+def create_skill(skill_data: UserSkillMetis):
+    return EmergencyCall(skill_data)
 
 
 class EmergencyCall(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+    def __init__(self, skill_data: UserSkillMetis):
+        MycroftSkill.__init__(self, skill_data=skill_data)
 
-    @intent_file_handler('call.emergency.intent')
-    def handle_call_emergency(self, message):
-        self.speak_dialog('call.emergency')
+    @intent_handler(IntentBuilder("EmergencyCallHelper").require("help"))
+    def handle_call(self, message, websocket):
+        call_api = skill_api(self.skill_data.identifier, self.skill_data.api_key, message.data["utterance"])
+        print('result call api ' + call_api)
+        print('Message Data Utterance ' + message.data["utterance"])
+        self.speak('Ich habe hilfe gerufen', websocket=websocket)
 
 
-def create_skill():
-    return EmergencyCall()
 
+def skill_api(identifier: str, api_key: str, text: str):
+    print ('skill_api')
+    #payload = {'customer_id' : data.customer_id, 'api_key': data.api_key}
+    payload = {
+        'identifier': identifier,
+        'api_key': api_key,
+        'utterance': text
+    }
+    #response = requests.get(url=skill.auth_skill_url, params=payload, json=dataRequest)
+    response = requests.post(url="http://80.158.34.90:8005/api/skill/help-call", params=payload)
+    print("request status code " + str(response.status_code))
+    print('skills ' + str(response.text))
+        
+    if response.status_code == 200:
+        #skill = add_skill_organistaion(user, data, db)                    
+        return str(response.text)
+    else:
+        return None
